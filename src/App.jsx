48024941a39c5251364ea55e125cf73bc202ec39
Sourabh-Chauhan <sourabhc2018@gmail.com>
2025-06-29 00:11:56 +0530

    Implement About Section with animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktail from "./components/Cocktail";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       {/* <div className="h-dvh bg-black"></div> */}
       <Cocktail />
+      <About />
     </main>
   );
 };
